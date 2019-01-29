8d5cd9c81ecc9c28526878d5c65e72421ae41444
Dominic Cayme <dcayme@gmail.com>
2019-01-29 22:59:45 +0700

    add Navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import SignIn from './components/SignIn/SignIn';
+import Navigation from './components/Navigation/Navigation';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <SignIn/>
+      <Navigation/>
       </div>
     );
   }
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navigation = () => {
+	return (
+		<div style={{ display:'flex', justifyContent:'flex-end'  }} >
+			<p className= 'f3 pa3 black underline pointer link dim'>
+				Sign Out
+			</p>
+		</div>)
+}
+
+export default Navigation;
