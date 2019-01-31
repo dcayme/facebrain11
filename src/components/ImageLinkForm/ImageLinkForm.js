688a73751da57d61bd86ff4f0e367c01ae4be783
Dominic Cayme <dcayme@gmail.com>
2019-01-31 23:21:16 +0700

    add input and button field

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './ImageLinkForm.css';
+
+const ImageLinkForm = () => {
+	return(
+		<div className='center'>	
+			<div className= 'Honey pa4 center br3 shadow-5'>
+				<input 
+					type="text"
+					className=''
+				/>
+				<button
+					className=''  
+				>
+					Detect
+				</button>
+			</div>
+		</div>
+	)
+}
+
+export default ImageLinkForm;
